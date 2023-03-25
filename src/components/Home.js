66cae140b91e03071e0dc23d283278a6e3cf3c09
irsat000 <irsat000@gmail.com>
2023-03-26 00:25:45 +0300

    homepage design - 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { GeoAltFill, Search } from "react-bootstrap-icons";
+import { GeoAltFill, Search, ChevronCompactUp } from "react-bootstrap-icons";
 //import logo from '../logo.svg';
+import W_Rain from "../assets/conditions/rain.png";
 
 
 export const Page_Home = () => {
@@ -16,10 +17,10 @@ export const Page_Home = () => {
                     <span>Bursa - Türkiye</span>
                 </div>
             </header>
-            <div className="today_info-cont">
+            <main className="today_info-cont">
                 <div className="weather_condition">
                     <div className="weather">
-                        <img src={``} alt="Weather condition" />
+                        <img src={W_Rain} alt="Weather condition" />
                     </div>
                     <div className="temperature">
                         <span>20°C</span>
@@ -28,13 +29,41 @@ export const Page_Home = () => {
                 <div className="extra_condition">
                     <div className="humidity">
                         <span>Humidity</span>
-                        <span>50%</span>
+                        <span className="value">50%</span>
                     </div>
                     <div className="windSpeed">
                         <span>Wind speed</span>
-                        <span>30 km/h</span>
+                        <span className="value">30 km/h</span>
                     </div>
                 </div>
+            </main>
+            <section className="hourly_weather">
+                <h6 className="todays_name">Tuesday</h6>
+                <ul className="hourly_list">
+                    <li>
+                        <span>8:00</span>
+                        <img src={W_Rain} alt="Hourly weather condition" />
+                        <span>24°</span>
+                    </li>
+                    <li>
+                        <span>12:00</span>
+                        <img src={W_Rain} alt="Hourly weather condition" />
+                        <span>28°</span>
+                    </li>
+                    <li>
+                        <span>17:00</span>
+                        <img src={W_Rain} alt="Hourly weather condition" />
+                        <span>18°</span>
+                    </li>
+                    <li>
+                        <span>21:00</span>
+                        <img src={W_Rain} alt="Hourly weather condition" />
+                        <span>16°</span>
+                    </li>
+                </ul>
+            </section>
+            <div className="weekly_report-cont">
+                <button className="show_weekly_report">Next 7 days <ChevronCompactUp className="icon" /></button>
             </div>
         </div>
     );
